eecd71d8816d01cf8b3f116264d24d01100594ed
HUNG-PC <hung@hung-pcs-mac.local>
2018-07-16 12:21:39 +0700

    Initial Commit

---
+++ Udder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Udder
+//
+//  Created by HUNG-PC on 7/16/18.
+//  Copyright © 2018 HUNG-PC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
